2ec64c0720bfca05b5d51aa21b1e8655200902d3
Tim <timothy_may@hotmail.co.uk>
2020-04-10 15:27:56 +0100

    Altered how user config directory is built
    Config directory now built within the users home directory

--- enigma/functions/config/find_config.py
+++ enigma/functions/config/find_config.py
@@ -1,22 +1,14 @@
 """Module for finding config files and directory"""
 
 from re import search
-from os import getcwd
+from os import path
 
 
 def get_path_of_user_config_directory():
 
-    wd = getcwd()
+    # TODO Get this function to work properly
 
-    path_to_remove = search("(?<=enigma\\\).+", wd)
-
-    if path_to_remove is None:
-
-        raise ValueError("No directory match found")
-
-    dir_path = wd.replace(path_to_remove.group(0), "")
-
-    dir_path = dir_path + "user_configs\\"
+    dir_path = path.expanduser("~\\enigma_user_configs\\")
 
     return dir_path
 
@@ -27,3 +19,14 @@ def get_path_from_file_name(file_name, dir_path):
     file_path = dir_path + f"{file_name}.pickle"
 
     return file_path
+
+
+if __name__ == "__main__":
+
+    dir_path_test = get_path_of_user_config_directory()
+
+    print(dir_path_test)
+
+    file_path_test = get_path_from_file_name("test", dir_path_test)
+
+    print(file_path_test)
